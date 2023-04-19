715e7cca81c60beab49c7956a1dd7321b2f74736
alexandrekonomi <alexandrekonomi1234@gmail.com>
2023-04-18 23:22:11 -0300

    add api userauth rest

---
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,53 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    @JsonIgnore
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String fullName;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+
+}
---
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -0,0 +1,23 @@
+package com.ead.authuser.services;
+
+import com.ead.authuser.models.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+
+
+    List<UserModel>findAll();
+
+    Optional<UserModel> findbyId(UUID id);
+
+    void delete(UserModel userModel);
+
+    void save(UserModel userModel);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
